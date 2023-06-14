6e82bcacb969e57f964ea1d13fa033e008cb8bf3
Perke1 <springerjan2@gmail.com>
2023-06-14 19:01:01 +0200

    refactor: moved AnimateIn component to the feature folder

---
+++ src/features/Animate/components/AnimateIn.tsx
@@ -0,0 +1,26 @@
+import { useRef, ReactNode } from "react";
+import { useElementOnScreen } from "../hooks/useElementOnScreen";
+
+export const AnimateIn = ({
+  children,
+  photos,
+}: {
+  children: ReactNode;
+  photos?: boolean;
+}) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const onScreen = useElementOnScreen(ref);
+  return (
+    <div
+      ref={ref}
+      className={`${onScreen && photos ? "photos-slide" : ""}`}
+      style={{
+        opacity: onScreen ? 1 : 0,
+        translate: onScreen ? "none" : "0 2rem",
+        transition: "600ms ease-in-out",
+      }}
+    >
+      {children}
+    </div>
+  );
+};
