28d2311e1f66680016ad08a4f65c78c1c2865e12
Perke1 <springerjan2@gmail.com>
2023-06-14 19:01:30 +0200

    refactor: moved animate feature hook to it's folder out of global hooks

---
+++ src/features/Animate/hooks/useElementOnScreen.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export const useElementOnScreen = (
+  ref: React.RefObject<Element>,
+  rootMargin = "0px"
+) => {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsIntersecting(entry.isIntersecting);
+      },
+      { rootMargin }
+    );
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
+  return isIntersecting;
+};
