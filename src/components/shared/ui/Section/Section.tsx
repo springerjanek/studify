6c960230c3e815390b57b02f2564997f5a260364
Perke1 <springerjan2@gmail.com>
2023-06-15 12:20:00 +0200

    feat: added Section Component

---
+++ src/components/shared/ui/Section/Section.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Section = styled.div`
+  display: flex;
+  align-items: center;
+  align-content: center;
+  gap: 250px;
+  font-size: 22px;
+  min-height: 100vh;
+
+  &:nth-child(1) {
+    margin-top: -100px;
+  }
+`;
