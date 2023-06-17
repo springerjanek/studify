7f331839b2b8e5c68a3f20e4a791d9dca50a342d
Perke1 <springerjan2@gmail.com>
2023-06-17 16:47:58 +0200

    feat: private route & added auth provider

--- src/app/Routing.tsx
+++ src/app/Routing.tsx
@@ -1,15 +1,35 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ReactNode } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { App } from "./App";
 import { Dashboard } from "@features/Dashboard";
+import { AuthProvider, useAuth } from "./shared/utils/auth";
+
+const PrivateRoute = ({ children }: { children: ReactNode }) => {
+  const { session } = useAuth();
+  if (!session) {
+    <Navigate to={"/"} />;
+  }
+
+  return children;
+};
 
 export const Routing = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<App />} />
-        <Route path="/" element={<App />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="*" element={<App />} />
+          <Route path="/" element={<App />} />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 };
