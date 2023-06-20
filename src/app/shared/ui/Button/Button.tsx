0febf98e72b5ea52623119aaec83e976ef8b6abb
Perke1 <springerjan2@gmail.com>
2023-06-20 19:00:50 +0200

    feat: added Button component

---
+++ src/app/shared/ui/Button/Button.tsx
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+
+export const Button = styled.button`
+  width: 35%;
+  padding: 12px;
+  font-family: "Raleway", sans-serif;
+  font-size: 17px;
+  font-weight: 600;
+  border-color: white;
+  border-radius: 8px;
+  background-color: white;
+  cursor: pointer;
+`;
