63a8440d84a73c03f3d9bf9685530e38e0f1a2cc
Perke1 <springerjan2@gmail.com>
2023-07-12 19:30:02 +0200

    refactor: changed hook name

---
+++ src/app/features/Dashboard/hooks/useGetDueAssignmentDates.ts
@@ -0,0 +1,8 @@
+import { Assignments } from "../components/Dashboard";
+
+export const useGetDueAssignmentsDates = (assignments: Assignments) => {
+  const dueAssignmentsDates = assignments.map((assignment) => {
+    return assignment.dueDate;
+  });
+  return dueAssignmentsDates;
+};
