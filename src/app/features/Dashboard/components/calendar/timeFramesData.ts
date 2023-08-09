6c43bd1bb477b95d717dcebcfe831dc012581ab9
Perke1 <springerjan2@gmail.com>
2023-08-09 16:11:51 +0200

    refactor: changed timeFrames to timeFramesData

---
+++ src/app/features/Dashboard/components/calendar/timeFramesData.ts
@@ -0,0 +1,13 @@
+export  const timeFramesData = [
+  "8AM-9AM",
+  "9AM-10AM",
+  "10AM-11AM",
+  "11AM-12PM",
+  "12PM-1PM",
+  "1PM-2PM",
+  "2PM-3PM",
+  "3PM-3:30PM",
+  "3:30PM-4PM",
+  "3PM-4PM",
+  "4PM-5PM",
+];
