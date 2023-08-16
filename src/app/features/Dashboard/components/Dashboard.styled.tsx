18f2e14f0a44ab96381057933a38bea5c77e227f
Perke1 <springerjan2@gmail.com>
2023-08-16 16:57:25 +0200

    refactor: fixed dashboard on mobile

--- src/app/features/Dashboard/components/Dashboard.styled.tsx
+++ src/app/features/Dashboard/components/Dashboard.styled.tsx
@@ -3,23 +3,28 @@ import "react-calendar/dist/Calendar.css";
 
 export const Container = styled.main`
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   padding-bottom: 20px;
+  width: 100%;
   @media only screen and (min-width: 300px) {
     flex-flow: column-reverse;
     align-items: center;
     gap: 50px;
     margin: 0px;
-    padding: 20px;
-    padding-top: 20px;
+    padding-top: 50px;
   }
 
   @media only screen and (min-width: 1000px) {
     flex-direction: row;
     align-items: flex-start;
+    gap: 70px;
+    padding: 20px;
   }
-  @media only screen and (min-width: 1200px) {
-    margin-left: 150px;
-    margin-right: 150px;
+  @media only screen and (min-width: 1440px) {
+    gap: 200px;
   }
+  @media only screen and (min-width: 1600px) {
+    gap: 400px;
+  }
+
 `;
