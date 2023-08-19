d7c0af4b8fc702414bff5587730576e107906ee9
Perke1 <springerjan2@gmail.com>
2023-08-19 17:16:57 +0200

    feat: added completeAssignment mutation

---
+++ src/app/features/Dashboard/data-access/mutations/completeAssignment.mutation.tsx
@@ -0,0 +1,43 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { supabase } from "../../../../supabase";
+import { UserScheduleResponse } from "../getUserSchedule.query";
+
+export const useCompleteAssignment = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async ({
+      userId,
+      assignmentName,
+    }: {
+      userId: string;
+      assignmentName: string;
+    }) => {
+      const scheduleQueryData: UserScheduleResponse | undefined =
+        queryClient.getQueryData(["user_schedule"]);
+
+      const userSchedule =
+        scheduleQueryData && scheduleQueryData.user_schedule[0].data;
+
+      if (userSchedule) {
+        const scheduleWithoutDeletedAsssignment =
+          userSchedule.assignments.filter((assignment) => {
+            return assignment.name !== assignmentName;
+          });
+
+        const updatedSchedule = {
+          assignments: scheduleWithoutDeletedAsssignment,
+        };
+
+        await supabase
+          .from("schedules")
+          .update({ data: updatedSchedule })
+          .eq("user_id", userId);
+      }
+    },
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["user_schedule"] });
+    },
+  });
+};
