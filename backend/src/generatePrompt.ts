13db8c9247e9018be87a9bafd01f4fff425a4631
Perke1 <springerjan2@gmail.com>
2023-08-25 15:44:21 +0200

    feat: added filtering past assignments

--- backend/src/generatePrompt.ts
+++ backend/src/generatePrompt.ts
@@ -1,11 +1,17 @@
 import { supabase } from "./supabase.ts";
 
+type Assignment = {
+  id: number;
+  name: string;
+  dueDate: string;
+};
+
 export async function generatePrompt({
-  assignment,
+  assignmentName,
   dueDate,
   user_id,
 }: {
-  assignment: string;
+  assignmentName: string;
   dueDate: string;
   user_id: string;
 }) {
@@ -20,42 +26,54 @@ export async function generatePrompt({
     ("0" + month).slice(-2),
     year,
   ].join(".");
+
   const currentHour = date.getHours() % 12 || 12;
+  const currentMinutes = date.getMinutes()
+  const currentTime = currentHour.toString() + ":" + currentMinutes.toString()
 
   const prefferedTime = "3-4";
-
-  let userAlreadyHaveWorkPlan = false;
   let prompt = "";
 
-  prompt += `Assignments:\n- [${assignment}]: Due on [${dueDate}]\n`;
+  prompt += `Assignments:\n- [${assignmentName}]: Due on [${dueDate}]\n`;
 
   const { data: goingAssignments, error } = await supabase
     .from("assignments")
     .select("*")
     .eq("user_id", user_id)
-    .neq("name", assignment);
+    .neq("name", assignmentName);
 
   if (error) {
     throw error;
   }
 
   if (goingAssignments.length > 0) {
-    userAlreadyHaveWorkPlan = true;
+
+    const filteredAssignments = goingAssignments.filter((goingAssignment: Assignment) => {
+      const splittedDate = goingAssignment.dueDate.split(".")
+      const [day, month, year] = splittedDate;
+      const formattedDate = `${month}/${day}/${year}`;
+
+       const dueDate = new Date(formattedDate);
+      const timeDiff = date.getTime() - dueDate.getTime()
+
+      return timeDiff <= 0
+    })
+
     prompt += "Going on assignments:\n";
-    goingAssignments.map((assignment) => {
+   filteredAssignments.map((assignment) => {
       prompt += `- [${assignment.name}]: Due on [${assignment.dueDate}]\n`;
     });
     prompt +=
-      `Current Time: ${currentHour}\n` +
+      `Current Time: ${currentTime}\n` +
       `Preffered Time: ${prefferedTime}\n` +
       `Please create a schedule between today: ${currentDate}, and the day before the due date for each assignment, considering the user's preferred time and the due dates\n END`;
   }
 
   if (goingAssignments.length === 0) {
     prompt +=
-      `Current Time: ${currentHour}\n` +
+      `Current Time: ${currentTime}\n` +
       `Preffered Time: ${prefferedTime}\n` +
       `Please create a schedule between today: ${currentDate}, and the day before the due date for each assignment, considering the user's preferred time and the due dates\n END`;
   }
-  return { prompt, userAlreadyHaveWorkPlan };
+  return { prompt };
 }
