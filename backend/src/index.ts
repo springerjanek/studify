e558a9f7fc3b2a0bbf1e3e614d3a8bac15588934
Perke1 <springerjan2@gmail.com>
2023-08-31 15:59:46 +0200

    refactor: added rate limit and changed error handling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,25 @@
 import express from "express";
 import * as dotenv from "dotenv";
+import rateLimit from "express-rate-limit";
 import { supabase } from "./supabase.ts";
 import { generatePrompt } from "./generatePrompt.ts";
 import { generateWorkPlan } from "./generateWorkPlan.ts";
+import { AssignmentExistsError, UserSchedule } from "./utilts.ts";
 
 dotenv.config();
+
+const limiter = rateLimit({
+  windowMs: 10 * 1000,
+  max: 1,
+  skip: (req) => req.method === "OPTIONS",
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 const app = express();
 
 app.use(express.json());
+app.use(limiter);
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5173");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
@@ -21,44 +32,81 @@ app.use(function (req, res, next) {
 
 app.post("/assign-work", async (req, res) => {
   try {
-    const { user_id, assignment, dueDate } = req.body;
-
-    const { prompt, userAlreadyHaveWorkPlan } = await generatePrompt({
-      assignment,
-      dueDate,
+    const {
       user_id,
-    });
+      assignmentName,
+      dueDate,
+      userSchedule,
+    }: {
+      user_id: string;
+      assignmentName: string;
+      dueDate: string;
+      userSchedule: UserSchedule;
+    } = req.body;
 
-    const workPlan = await generateWorkPlan(prompt);
+    const { data: selectAssignment } = await supabase
+      .from("assignments")
+      .select("*")
+      .eq("name", assignmentName);
 
-    if (!userAlreadyHaveWorkPlan && workPlan) {
-      const { data, error: insertError } = await supabase
-        .from("schedules")
-        .insert({
-          user_id: user_id,
-          data: JSON.parse(workPlan),
-        });
+    if (selectAssignment?.length === 0) {
+      const { error: insertError } = await supabase.from("assignments").insert({
+        user_id: user_id,
+        name: assignmentName,
+        dueDate: dueDate,
+      });
+
       if (insertError) {
-        throw insertError;
+        throw new Error("Error during inserting into DB");
       }
-    }
 
-    if (userAlreadyHaveWorkPlan && workPlan) {
-      const { data, error: updateError } = await supabase
-        .from("schedules")
-        .update({
-          data: JSON.parse(workPlan),
-        })
-        .eq("user_id", user_id);
-      if (updateError) {
-        throw updateError;
+      const { prompt } = await generatePrompt({
+        assignmentName,
+        dueDate,
+        user_id,
+      });
+
+      const workPlan = await generateWorkPlan(prompt);
+
+      if (workPlan) {
+        if (
+          userSchedule.length > 0 &&
+          userSchedule[0].data.assignments.length >= 0
+        ) {
+          const { data, error: updateError } = await supabase
+            .from("schedules")
+            .update({
+              data: JSON.parse(workPlan),
+            })
+            .eq("user_id", user_id);
+          if (updateError) {
+            throw updateError;
+          }
+        }
+
+        if (userSchedule.length === 0) {
+          const { data, error: insertError } = await supabase
+            .from("schedules")
+            .insert({
+              user_id: user_id,
+              data: JSON.parse(workPlan),
+            });
+          if (insertError) {
+            throw insertError;
+          }
+        }
       }
+    } else {
+      throw new AssignmentExistsError();
     }
 
     res.send("Successfully assigned work plan");
   } catch (error) {
-    console.error("Error assigning work:", error);
-    res.status(500).json({ error: "Failed to assign work" });
+    if (error instanceof AssignmentExistsError) {
+      res.status(400).json({ error: error.message });
+    } else {
+      res.status(500).json({ error: "Failed to assign work" });
+    }
   }
 });
 
