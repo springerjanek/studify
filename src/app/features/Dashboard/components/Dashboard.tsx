14066b5d7fe3a5a3f7a4581f5d5d5cbe5fe047b4
Perke1 <springerjan2@gmail.com>
2023-09-24 09:39:15 +0200

    refactor: added context functions

--- src/app/features/Dashboard/components/Dashboard.tsx
+++ src/app/features/Dashboard/components/Dashboard.tsx
@@ -1,6 +1,7 @@
 import { useGetUserAssignments } from "../data-access/getUserAssignments.query";
 import { useGetUserSchedule } from "../data-access/getUserSchedule.query";
 import { useAuth } from "@shared/utils/auth";
+import { useCalendarModal } from "@/app/shared/utils/calendarModal";
 import Calendar from "react-calendar";
 import { CalendarDayModal } from "./calendar/modals/calendarDay/CalendarDayModal";
 import { CalendarTile } from "./calendar/CalendarTile";
@@ -8,7 +9,6 @@ import { AssignmentsList } from "./assignments/AssignmentsList";
 import { Container } from "./Dashboard.styled";
 import { ModalContainer } from "./calendar/modals/calendarDay/calendarDay.styled";
 import { DashboardLayout } from "@/app/layouts/DashboardLayout";
-import { useCalendarModal } from "@/app/shared/utils/calendarModal";
 
 type Assignment = {
   id: number;
@@ -19,47 +19,48 @@ type Assignment = {
 export type Assignments = Assignment[];
 
 export const Dashboard = () => {
-
-    const {
-      showCalendarDayModal,
-      setShowCalendarDayModal,
-      dataToDayModal, 
-      handleOpenCalendarDayModal,
-    } = useCalendarModal();
+  const {
+    showCalendarDayModal,
+    setShowCalendarDayModal,
+    dataToDayModal,
+    handleOpenCalendarDayModal,
+  } = useCalendarModal();
 
   const { currentUser } = useAuth();
 
   const { data: user_assignments } = useGetUserAssignments(currentUser!.id);
   const { data: user_schedule } = useGetUserSchedule(currentUser!.id);
- 
+
   return (
-      <DashboardLayout>
-        <Container>
-          <AssignmentsList user_assignments={user_assignments} />
+    <DashboardLayout>
+      <Container>
+        <AssignmentsList user_assignments={user_assignments} />
 
-          {showCalendarDayModal && (
-            <ModalContainer>
-              <CalendarDayModal
-                showModal={setShowCalendarDayModal}
-                data={dataToDayModal}
-              />
-            </ModalContainer>
+        {showCalendarDayModal && (
+          <ModalContainer>
+            <CalendarDayModal
+              showModal={setShowCalendarDayModal}
+              data={dataToDayModal}
+            />
+          </ModalContainer>
+        )}
+
+        <Calendar
+          onClickDay={(value) =>
+            handleOpenCalendarDayModal({ date: value, userId: currentUser.id })
+          }
+          showNeighboringMonth={false}
+          tileContent={({ date }) => (
+            <CalendarTile
+              date={date}
+              assignments={user_assignments}
+              schedule={user_schedule}
+            />
           )}
-
-          <Calendar
-            onClickDay={(value) => handleOpenCalendarDayModal({date: value, userId: currentUser.id})}
-            showNeighboringMonth={false}
-            tileContent={({ date }) => (
-              <CalendarTile
-                date={date}
-                assignments={user_assignments}
-                schedule={user_schedule}
-              />
-            )}
-            view="month"
-            locale="en-EN"
-          />
-        </Container>
-      </DashboardLayout>
+          view="month"
+          locale="en-EN"
+        />
+      </Container>
+    </DashboardLayout>
   );
 };
