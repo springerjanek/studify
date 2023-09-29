9d96505363e4d5f3d177ce57f4f5303abfa47e71
Perke1 <springerjan2@gmail.com>
2023-09-29 20:55:32 +0200

    feat: added notification route

--- src/app/Routing.tsx
+++ src/app/Routing.tsx
@@ -2,8 +2,9 @@ import { ReactNode } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { useAuth } from "./shared/utils/auth";
 import { Home } from "./Home";
-import { Dashboard } from "@features/Dashboard";
 import { SignIn } from "@features/Auth/containers/Login";
+import { Dashboard } from "@features/Dashboard";
+import { Notifications } from "@features/Dashboard/components/notifications/Notifications";
 
 const PrivateRoute = ({ children }: { children: ReactNode }) => {
   const { session } = useAuth();
@@ -23,6 +24,14 @@ export const Routing = () => {
           element={
             <PrivateRoute>
               <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/dashboard/notifications"
+          element={
+            <PrivateRoute>
+              <Notifications />
             </PrivateRoute>
           }
         />
