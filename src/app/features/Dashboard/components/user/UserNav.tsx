2bb0b078da739bb18652bac0466652cae366648a
Perke1 <springerjan2@gmail.com>
2023-11-06 17:48:01 +0100

    refactor: getting real-time user avatar

--- src/app/features/Dashboard/components/user/UserNav.tsx
+++ src/app/features/Dashboard/components/user/UserNav.tsx
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "@/app/shared/utils/auth";
 import { useGetUserName } from "../../data-access/getUserName.query";
 import { useGetUserAvatar } from "../../data-access/getUserAvatar.query";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -18,11 +18,11 @@ export const UserNav = () => {
   const navigate = useNavigate();
   const { currentUser } = useAuth();
 
-  const {data: userAvatar } = useGetUserAvatar(currentUser.id)
+  const { data: userAvatar } = useGetUserAvatar(currentUser.id);
   const { data: name } = useGetUserName(currentUser.id);
 
-  const avatarUrl = userAvatar
-    ? userAvatar[0].avatar_url
+  const userAvatarUrl = userAvatar
+    ? `https://rwwldaqpuxdnztewxate.supabase.co/storage/v1/object/public/avatars/${userAvatar[0].avatar_url}`
     : "https://static-00.iconduck.com/assets.00/profile-circle-icon-512x512-zxne30hp.png";
   const userName = name ? name[0].name : "User";
 
@@ -32,10 +32,9 @@ export const UserNav = () => {
         <Button variant="ghost" className="relative h-11 w-11 rounded-full">
           <Avatar className="h-9 w-9">
             <AvatarImage
-              src={`https://rwwldaqpuxdnztewxate.supabase.co/storage/v1/object/public/avatars/${avatarUrl}`}
+              src={userAvatarUrl}
               alt="User Image"
             />
-            <AvatarFallback>SC</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
