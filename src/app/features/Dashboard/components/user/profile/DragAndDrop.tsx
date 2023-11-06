fecce441e1552d62638dc6e65ad87a1d234335fc
Perke1 <springerjan2@gmail.com>
2023-11-06 17:47:33 +0100

    refactor: correctly displaying and changing avatar functionality

--- src/app/features/Dashboard/components/user/profile/DragAndDrop.tsx
+++ src/app/features/Dashboard/components/user/profile/DragAndDrop.tsx
@@ -1,12 +1,24 @@
 import { useRef, useState } from "react";
-import CloseIcon from "@mui/icons-material/Close";
+import { useGetUserAvatar } from "../../../data-access/getUserAvatar.query";
+import { updateAvatarLogic } from "./utils/updateAvatar";
+import { User } from "@supabase/supabase-js";
 import { AvatarContainer, DragAndDropContainer } from "./Profile.styled";
+import { Button } from "@/app/shared/ui/Button";
+import CloseIcon from "@mui/icons-material/Close";
 
-export const DragAndDrop = () => {
+export const DragAndDrop = ({ currentUser }: { currentUser: User }) => {
+  const [saveAvatar, setSaveAvatar] = useState(false);
   const [selectedImage, setSelectedImage] = useState<null | File>(null);
+  const [isDragActive, setIsDragActive] = useState<boolean>(false);
+
+  const { data: userAvatar } = useGetUserAvatar(currentUser.id);
+  const userAvatarUrl = userAvatar
+    ? `https://rwwldaqpuxdnztewxate.supabase.co/storage/v1/object/public/avatars/${userAvatar[0].avatar_url}`
+    : "https://static-00.iconduck.com/assets.00/profile-circle-icon-512x512-zxne30hp.png";
+
   const fileInputRef = useRef<HTMLInputElement | null>(null);
 
-  const [isDragActive, setIsDragActive] = useState<boolean>(false);
+  updateAvatarLogic({ selectedImage, saveAvatar, currentUser });
 
   const handleDragEnter = () => {
     setIsDragActive(true);
@@ -24,7 +36,9 @@ export const DragAndDrop = () => {
     e.preventDefault();
     setIsDragActive(false);
     const files = Array.from(e.dataTransfer.files);
-    setSelectedImage(files[0]);
+    const file = files[0];
+
+    setSelectedImage(file);
   };
 
   const openFileInput = () => {
@@ -40,35 +54,45 @@ export const DragAndDrop = () => {
     }
   };
 
+  const handleCloseIcon = () => {
+    setSelectedImage(null);
+    setSaveAvatar(false);
+  };
+
   return (
-    <DragAndDropContainer
-      onDragEnter={handleDragEnter}
-      onDragLeave={handleDragLeave}
-      onDragOver={handleDragOver}
-      onDrop={handleDrop}
-    >
-      <input
-        type="file"
-        accept="image/*"
-        style={{ display: "none" }}
-        ref={fileInputRef}
-        onChange={handleFileInputChange}
-      />
-      <CloseIcon onClick={() => setSelectedImage(null)} className="w-4 h-4" />
+    <>
+      <DragAndDropContainer
+        onDragEnter={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
+      >
+        <input
+          type="file"
+          accept="image/*"
+          style={{ display: "none" }}
+          ref={fileInputRef}
+          onChange={handleFileInputChange}
+        />
+        <CloseIcon onClick={handleCloseIcon} className="w-4 h-4" />
 
-      <AvatarContainer>
-        <img
-          className="rounded-full cursor-pointer"
-          onClick={openFileInput}
-          src={
-            selectedImage
-              ? URL.createObjectURL(selectedImage)
-              : "https://static-00.iconduck.com/assets.00/profile-circle-icon-512x512-zxne30hp.png"
-          }
-        ></img>
-      </AvatarContainer>
+        <AvatarContainer>
+          <img
+            className="rounded-full cursor-pointer"
+            onClick={openFileInput}
+            src={
+              selectedImage ? URL.createObjectURL(selectedImage) : userAvatarUrl
+            }
+          ></img>
+        </AvatarContainer>
 
-      {isDragActive && <p>Drag and drop!</p>}
-    </DragAndDropContainer>
+        {isDragActive && <p>Drag and drop!</p>}
+      </DragAndDropContainer>
+      {selectedImage && (
+        <Button className="mt-4" $primary onClick={() => setSaveAvatar(true)}>
+          Save Avatar
+        </Button>
+      )}
+    </>
   );
 };
