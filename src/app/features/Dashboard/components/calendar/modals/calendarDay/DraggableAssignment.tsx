7f7297c1f19ab6d85c8973552146cc1d7f055741
Perke1 <springerjan2@gmail.com>
2023-11-28 17:40:02 +0100

    feat: added pencil icon to edit timeFrame

--- src/app/features/Dashboard/components/calendar/modals/calendarDay/DraggableAssignment.tsx
+++ src/app/features/Dashboard/components/calendar/modals/calendarDay/DraggableAssignment.tsx
@@ -1,5 +1,8 @@
-import { Draggable } from 'react-beautiful-dnd';
-import { DraggableWrapper } from './calendarDay.styled';
+import { useState } from "react";
+import { Draggable } from "react-beautiful-dnd";
+import { DraggableWrapper } from "./calendarDay.styled";
+import { TimePicker } from "../TimePicker";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 
 export const DraggableAssignment = ({
   frameMatchingAssignments,
@@ -13,6 +16,11 @@ export const DraggableAssignment = ({
     | undefined;
   currentDayDate: string;
 }) => {
+  const [editTimeRange, setEditTimeRange] = useState(false);
+
+  const closeEdit = () => {
+    setEditTimeRange(false);
+  };
   return (
     <>
       {frameMatchingAssignments &&
@@ -40,7 +48,18 @@ export const DraggableAssignment = ({
                     ...provided.draggableProps.style,
                   }}
                 >
-                  {matchingAssignment.name} {matchingTimeFrame}
+                  {matchingAssignment.name} {""}
+                  {matchingTimeFrame}
+                  <EditOutlinedIcon
+                    className="w-1 h-1"
+                    onClick={() => setEditTimeRange(true)}
+                  />
+                  {editTimeRange && (
+                    <TimePicker
+                      timeFrame={matchingTimeFrame!}
+                      closeEdit={closeEdit}
+                    />
+                  )}
                 </DraggableWrapper>
               )}
             </Draggable>
