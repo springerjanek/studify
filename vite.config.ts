4a0f8e2d8d382ca0087cae67aad54051e1a4552a
Perke1 <springerjan2@gmail.com>
2023-12-17 16:44:32 +0100

    feat: added testing config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,26 @@
 import { defineConfig } from "vite";
+import type { UserConfig as VitestUserConfig } from "vitest/config";
+
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
 
-// https://vitejs.dev/config/
+declare module "vite" {
+  export interface UserConfig {
+    test: VitestUserConfig["test"];
+  }
+}
+
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/app/testsUtils.tsx",
+    css: true
+  },
   define: {
-     global: {},
+    global: {},
   },
   resolve: {
     alias: {
